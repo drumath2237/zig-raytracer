bd7972e69d724875e5f6c90325ac8967b9ebb020
drumath <2237k.drumath@gmail.com>
2022-07-19 01:38:08 +0900

    :sparkles: ファイル分割テスト

--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,13 @@
 const std = @import("std");
 
+const add = @import("add.zig").add;
+
 pub fn main() anyerror!void {
     // Note that info level log messages are by default printed only in Debug
     // and ReleaseSafe build modes.
     std.log.info("All your codebase are belong to us.", .{});
+
+    std.debug.print("{d}", .{add(1, 2)});
 }
 
 test "basic test" {
